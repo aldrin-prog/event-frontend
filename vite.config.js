e6a03be6ee5ca2d27c9bf2b2c9c41252ebb7e452
Aldrin Lobis <aldrinlobi1@gmail.com>
2024-12-20 21:30:45 +0800

    edit base in vercel confi

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/',
+  base:'https://event-frontend-gold.vercel.app',
   define: {
     global: 'window', // Polyfill the global variable
   }
